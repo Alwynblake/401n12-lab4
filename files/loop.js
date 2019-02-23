474f19137f32dd9c73c50b4ac2d97c31b4368d4f
Alistair Blake <consultations@me.com>
2019-02-22 16:48:57 -0800

    example code

--- files/loop.js
+++ files/loop.js
@@ -1,22 +1,40 @@
 'use script';
 
 const os = require('os');
+const fs = require('fs');
 console.log(os.endiannes());
 
 
-//let arr = ['Larry', 'Moe', 'Curly'];
-//for ( let )
-let buffer = Buffer.from ('Larry');
-console.log (buffer);
+//example
+let buffer = Buffer.from('John');
+console.log(buffer);
+console.log(buffer.toString());
 
-let stringifyBuffer = buffer => {
-  let str = ' ';
-  //run a for loop for each character in the string of bytes
-  for ( let char of buffer ) {
+buffer[0] = 0x5A;
+console.log(buffer);
+// convert buffer back to a string
+console.log(buffer.toString());
+
+buffer[1] = 97;
+console.log(buffer);
+console.log(buffer.toString());
+
+// iterate each char in the array of bytes. convert buffer back to a string
+const stringify = (buffer) => {
+  let str = '';
+  for(let char of buffer) {
     str += String.fromCharCode(char);
   }
   return str;
 };
-// read the buffer and convert it to an integer
-console.log (buffer.readInt32LE(1));
 
+console.log( stringify(buffer) );
+// read the buffer and convert it to an integer
+console.log( buffer.readInt16LE(0));
+console.log( buffer.readInt16BE(3));
+
+// write
+fs.writeFile('./main.js', buffer, (err,data) => {
+  if (err ) { throw err; }
+  console.log(data);
+});
